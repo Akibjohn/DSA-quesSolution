4a53f12a8bceef5125fe3990b5b01f0d611f6156
Akibjohn <akibmohd285@gmail.com>
2022-09-19 16:21:05 +0530

    recursion

--- DSA+java/Code/Recursion/l001.java
+++ DSA+java/Code/Recursion/l001.java
@@ -80,6 +80,60 @@ public class l001{
         return fibonacci(n-1)+fibonacci(n-2);
     }
 
+    
+
+    public static int firstIndexReversed(int[] arr, int idx, int x) {
+        if(idx == -1) return -1;
+        int faith = firstIndexReversed(arr, idx - 1, x);
+        if(faith == -1) {
+            if(arr[idx] == x) return idx;
+            else return -1; 
+        } else {
+            return faith;
+        }
+    }
+
+    public static ArrayList<String> subSequence(String str) {
+        
+        if(str.length() == 0) {
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+        
+        ArrayList<String> faith = subSequence(str.substring(1));
+        ArrayList<String> ans = new ArrayList<>();
+        for(String fvs : faith) ans.add(fvs);
+        for(String fvs : faith) ans.add(str.charAt(0) + fvs);
+        return ans;
+    }
+
+    public static int firstIndex(int[] arr, int idx, int x){
+        if(idx == arr.length) return -1;
+        if(arr[idx] == x) return idx;
+        return firstIndex(arr, idx + 1, x);
+    }
+
+    public static ArrayList<String> gsp(int n) {
+
+        if(n <= 0) {
+            ArrayList<String> base = new ArrayList<>();
+            if(n == 0) base.add("");
+            return base;
+        }
+
+        ArrayList<String> f1 = gsp(n - 1);
+        ArrayList<String> f2 = gsp(n - 2);
+        ArrayList<String> f3 = gsp(n - 3);
+
+        ArrayList<String> ans = new ArrayList<>();
+        for(String str : f1) ans.add(str + '1');
+        for(String str : f2) ans.add(str + '2');
+        for(String str : f3) ans.add(str + '3');
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         // int x=2;
         // int n=5;
@@ -94,6 +148,7 @@ public class l001{
         // pattern(n);
         // System.out.println(power(x, n));
         // power(x, n, 1);;
-        System.out.println(fibonacci(7));
+        // System.out.println(fibonacci(7));
+        
     }
 }
